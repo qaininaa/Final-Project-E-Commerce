b1ab71c78362fb78ed79bcfd4c45c9e0a1127561
qaininaa <karinaghsn@gmail.com>
2024-12-11 15:21:20 +0700

    fix auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,7 @@ import axiosInstance from "../api/axiosInstance";
 
 export const fetchLogin = async (data, callback) => {
   try {
-    const res = await axiosInstance.post("https://fakestoreapi.com/auth/login", data);
+    const res = await axiosInstance.post("/auth/login", data);
     callback(true, res.data.token);
   } catch (err) {
     callback(false, err);

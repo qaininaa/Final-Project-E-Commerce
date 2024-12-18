94133355fac5c643ae0d3b58d75c987607d43615
qaininaa <karinaghsn@gmail.com>
2024-12-18 15:11:57 +0700

    add footer

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -10,10 +10,11 @@ import { MdOutlineCancel } from "react-icons/md";
 import Swal from "sweetalert2";
 import { CategoryCon } from "../components/context/CategoryContext";
 import { filterProduct, searchProduct } from "../redux/actions/products-action";
+import FooterPage from "../components/Fragments/FooterPage";
 
 const CartPage = () => {
   const carts = useSelector((state) => state.carts.cart);
-  const products = useSelector((state) => state.products);
+  const products = useSelector((state) => state.products.data);
   const [totalPrice, setTotalPrice] = useState(0);
   const [valid, setValid] = useState(true);
   const dispatch = useDispatch();
@@ -36,6 +37,10 @@ const CartPage = () => {
       sum += carts[i].qty * carts[i].price;
     }
     setTotalPrice(sum);
+  }, [carts]);
+
+  useEffect(() => {
+    console.log(carts);
   }, [carts]);
 
   useEffect(() => {
@@ -74,7 +79,6 @@ const CartPage = () => {
     }).then((result) => {
       const newval = category.split(" ");
       if (result.isConfirmed) {
-        console.log("cat dari cart", category);
         dispatch(checkoutCart());
         dispatch(filterProduct(category.toLowerCase())) ||
           dispatch(searchProduct(newval));
@@ -85,112 +89,115 @@ const CartPage = () => {
   };
 
   return (
-    <div className="p-2">
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th></th>
-            <th></th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-            <th>Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {carts.length > 0 &&
-            carts.map((item) => (
-              <tr key={item.id} className="text-xs text-center">
-                <td>
-                  <img src={item.image} alt="" className="w-10 h-14" />
-                </td>
-                <td>
-                  {item.title.substring(0, 20)}... <br />
-                  {!item.isValid && (
-                    <span className="text-red-500">Stock is not enough</span>
-                  )}
-                </td>
-                <td>
-                  {item.price.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  })}
-                </td>
-                <td>
-                  <input
-                    type="number"
-                    name=""
-                    id=""
-                    value={item.qty}
-                    onChange={(e) => {
-                      const newQuantity = parseInt(e.target.value);
-                      handleQuantityChange(item, newQuantity);
-                    }}
-                    min="1"
-                    className="border-2 border-slate-300 w-1/3 text-center"
-                  />
-                </td>
-                <td>
-                  {(item.price * item.qty).toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  })}
-                </td>
-                <td>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      swal
-                        .fire({
-                          title: "Are you sure?",
-                          text: "You won't be able to revert this!",
-                          icon: "warning",
-                          showCancelButton: true,
-                          confirmButtonText: "Yes, delete it!",
-                          cancelButtonText: "No, cancel!",
-                          reverseButtons: true,
-                        })
-                        .then((result) => {
-                          if (result.isConfirmed) {
-                            swal.fire({
-                              title: "Deleted!",
-                              text: "Your file has been deleted.",
-                              icon: "success",
-                            });
-                            dispatch(removeCart(carts, item));
-                          }
-                        });
-                    }}
-                  >
-                    <MdOutlineCancel size={25} color="red" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          <tr className="text-xs text-center">
-            <td colSpan={4}>Total</td>
-            <td>
-              {totalPrice.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div className="w-full flex justify-end px-3 mt-5">
-        <button
-          type="button"
-          className={`p-2 ${
-            !valid ? "bg-slate-400 text-white" : "bg-blue-400"
-          } `}
-          disabled={!valid}
-          onClick={handleCheckout}
-        >
-          Checkout
-        </button>
+    <>
+      <div className="p-2">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {carts.length > 0 &&
+              carts.map((item) => (
+                <tr key={item.id} className="text-xs text-center">
+                  <td>
+                    <img src={item.image} alt="" className="w-10 h-14" />
+                  </td>
+                  <td>
+                    {item.title.substring(0, 20)}... <br />
+                    {!item.isValid && (
+                      <span className="text-red-500">Stock is not enough</span>
+                    )}
+                  </td>
+                  <td>
+                    {item.price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      name=""
+                      id=""
+                      value={item.qty}
+                      onChange={(e) => {
+                        const newQuantity = parseInt(e.target.value);
+                        handleQuantityChange(item, newQuantity);
+                      }}
+                      min="1"
+                      className="border-2 border-slate-300 w-1/3 text-center"
+                    />
+                  </td>
+                  <td>
+                    {(item.price * item.qty).toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        swal
+                          .fire({
+                            title: "Are you sure?",
+                            text: "You won't be able to revert this!",
+                            icon: "warning",
+                            showCancelButton: true,
+                            confirmButtonText: "Yes, delete it!",
+                            cancelButtonText: "No, cancel!",
+                            reverseButtons: true,
+                          })
+                          .then((result) => {
+                            if (result.isConfirmed) {
+                              swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success",
+                              });
+                              dispatch(removeCart(carts, item));
+                            }
+                          });
+                      }}
+                    >
+                      <MdOutlineCancel size={25} color="red" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            <tr className="text-xs text-center">
+              <td colSpan={4}>Total</td>
+              <td>
+                {totalPrice.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="w-full flex justify-end px-3 mt-5">
+          <button
+            type="button"
+            className={`p-2 ${
+              !valid ? "bg-slate-400 text-white" : "bg-blue-400"
+            } `}
+            disabled={!valid}
+            onClick={handleCheckout}
+          >
+            Checkout
+          </button>
+        </div>
       </div>
-    </div>
+      <FooterPage />
+    </>
   );
 };
 

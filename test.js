41622308f7974bb9ca4bb0267c5d6c0252c97ac6
qaininaa <karinaghsn@gmail.com>
2024-12-20 18:30:44 +0700

    delete test

--- test.js
+++
@@ -1,6 +0,0 @@
-import {jwtDecode} from 'jwt-decode'
-
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXIiOiJqb2huZCIsImlhdCI6MTczNDE0Njc2OX0.B0t2fwp0yIyfFFXVSHG9jldTJGD2IZL6VC8OUKjfSsI'
-const decoded = jwtDecode(token);
-
-console.log(decoded);
